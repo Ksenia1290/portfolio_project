a70347c031fab375132a71fd82e84616cb599799
Ksenia1290 <Ksu.tender@yandex.ru>
2023-11-12 20:55:30 +0300

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import './App.css';
-import styled from 'styled-components';
+import { Header } from './layout/header/Header';
 
 
 function App() {
     return (
         <div className="App">
-           <Title>it</Title>
+          <Header/>
         </div>
     );
 }
@@ -14,8 +14,4 @@ function App() {
 
 export default App;
 
-const Title = styled.h1`
-    font-size: 1.5em;
-    text-align: center;
-    color: #e91e63;
-`;
+
