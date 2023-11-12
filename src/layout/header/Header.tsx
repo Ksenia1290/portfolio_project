59158b96df894253ef02914bd70a7597c1e19fe6
Ksenia1290 <Ksu.tender@yandex.ru>
2023-11-12 20:50:45 +0300

    1

---
+++ src/layout/header/Header.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export const Header =() => {
+    return (
+       <header>
+        <a href="">
+            <img src="" alt=""/>
+        </a>
+        <nav>
+            <ul>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+            </ul>
+        </nav>
+       </header>
+    );
+};
+
