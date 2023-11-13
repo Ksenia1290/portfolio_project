b3a4b23665a33c5e22e3684be657a0b12157c221
Ksenia1290 <Ksu.tender@yandex.ru>
2023-11-13 23:54:15 +0300

    1

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -1,13 +1,22 @@
 import React from "react";
+import styled from "styled-components";
+import photo from '../../../assets/images/photo.jpeg'
 
 export const Main = () => {
     return (
 <div>
     <div>
-        <span>
-            
-        </span>
+        <span>Hi There</span>
+        <h2>I am Ksenia Baguzova</h2>
+        <h1>A Web Developer</h1>
     </div>
+    <Photo src={photo} alt=""/>
 </div>
     );
 };
+
+const Photo = styled.img`
+width: 350px;
+height:430px;
+object-fit: cover;
+`
