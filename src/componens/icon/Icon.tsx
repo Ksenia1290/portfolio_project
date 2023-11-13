a4c6d6099a5ffe0138226f9d60b2fa5fcf750c16
Ksenia1290 <Ksu.tender@yandex.ru>
2023-11-13 23:06:12 +0300

    1

--- src/componens/icon/Icon.tsx
+++ src/componens/icon/Icon.tsx
@@ -3,11 +3,14 @@ import iconsSprire from '../../assets/images/icons-sprite.svg'
 
 type IconPropsType ={
     iconId:string
+    while? : string
+    height?:string
+    viewBox?:string
 }
 
 export const Icon = (props: IconPropsType) => {
     return (
-<svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+<svg width={props.while || "50"}  height={props.height || "50"} viewBox={props.viewBox ||  "0 0 50 50"} fill="none" xmlns="http://www.w3.org/2000/svg">
     <use xlinkHref={ `${iconsSprire}#${props.iconId}` } />
 </svg>
     );
