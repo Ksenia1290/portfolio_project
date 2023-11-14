7d50f96687f0f5abad447bf659392f78f7fd72f3
Ksenia1290 <Ksu.tender@uandex.ru>
2023-11-14 22:23:52 +0300

    1

---
+++ src/componens/menu/FlexWrapper.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+type FlexWrapperPropsType = {
+    direction?: string
+    justify?: string
+    align?:string
+    wrap?: string
+}
+
+const FlexWrapper = styled.div`
+display:flex;
+`
