01ff9c63180a54945558eb2b74b7557df64d8ec2
Ksenia1290 <Ksu.tender@yandex.ru>
2023-11-16 01:09:52 +0300

    1

--- src/layout/sections/works/Works.tsx
+++ src/layout/sections/works/Works.tsx
@@ -4,6 +4,10 @@ import { SectionTitle } from "../../../componens/SectionTitle";
 import { Menu } from "../../../componens/menu/Menu";
 import { FlexWrapper } from "../../../componens/FlexWrapper";
 import { Work } from "./work/Work";
+import socialImg from './../../../assets/images/social network.png'
+import timerImg from './../../../assets/images/new york.png'
+
+
 
 const worksItems =["ALL","LANDING PAGE","REACT","SPA"]
 
@@ -13,8 +17,13 @@ export const Works = () =>{
       <SectionTitle>My Works</SectionTitle>
       <Menu menuItems={worksItems}/>
       <FlexWrapper>
-        <Work/>
-        <Work/>
+        <Work title={"Social Network"} 
+              src={socialImg}
+              text={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit."}/>
+        
+        <Work title={"Timer"} 
+              src={timerImg}
+              text={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit  ut labore et dolore magna aliqua Ut enim"}/>
       </FlexWrapper>
 </StyledWorks>
     );
--- src/layout/sections/works/work/Work.tsx
+++ src/layout/sections/works/work/Work.tsx
@@ -1,21 +1,37 @@
 import React from "react";
 import styled from "styled-components";
 
-export const Work = () => {
+type WorkPropsType = {
+title:string
+text:string
+src:string
+}
+
+export const Work = (props:WorkPropsType) => {
     return(
 <StyledWork>
-    <Image src="" alt=""/>
-    <Title></Title>
-    <Text></Text>
-    <link></link>
+    <Image src={props.src} alt=""/>
+    <Title>{props.title}</Title>
+    <Text>{props.text}</Text>
+    <link href={"#"}>demo</link>
+    <link href={"#"}>code</link>
 </StyledWork>
     );
 };
 
 const StyledWork = styled.div`
-    
+    background-color: #ffe869;
 `
 
 const Image =styled.img`
+    
+`
+const Link =styled.a`
+    
+`
+const Title =styled.h3`
+    
+`
+const Text =styled.p`
     
 `
