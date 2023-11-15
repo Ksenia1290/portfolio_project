088d9396b0802ccd7d044e18367a3f0f339e4460
Ksenia1290 <Ksu.tender@yandex.ru>
2023-11-15 23:42:10 +0300

    1

--- src/layout/sections/skills/skill/Skill.tsx
+++ src/layout/sections/skills/skill/Skill.tsx
@@ -20,7 +20,9 @@ export const Skill = (props: SkillPropsType) =>{
 };
 
 const StyledSkill = styled.div`
-width: 33%;
+width: 30%;
+background-color: rgba(255, 255, 255, 0.56);
+margin:10px;
 `
 
 const SkillTitle = styled.h3`
