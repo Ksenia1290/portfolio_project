d6b9628f997c00729ac2b5161f41bdcea52ea7a6
Ksenia1290 <Ksu.tender@yandex.ru>
2023-11-16 00:36:31 +0300

    1

--- src/layout/sections/works/Works.tsx
+++ src/layout/sections/works/Works.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import styled from "styled-components";
 import { SectionTitle } from "../../../componens/SectionTitle";
+import { Menu } from "../../../componens/menu/Menu";
+
+const worksItems =["ALL","LANDING PAGE","REACT","SPA"]
 
 export const Works = () =>{
     return(
 <StyledWorks>
       <SectionTitle>My Works</SectionTitle>
+      <Menu menuItems={worksItems}/>
 </StyledWorks>
     );
 };
