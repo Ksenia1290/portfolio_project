2444c30799fc4b6657bca9fe9b584538e7457715
Ksenia1290 <Ksu.tender@yandex.ru>
2023-11-15 23:50:31 +0300

    1

--- src/layout/sections/skills/Skills.tsx
+++ src/layout/sections/skills/Skills.tsx
@@ -9,7 +9,7 @@ export const Skills = () => {
     return(
        <StyledSkills>
            <SectionTitle>My Skills</SectionTitle>
-           <FlexWrapper wrap={"wrap"}>
+           <FlexWrapper wrap={"wrap"} justify={"space-between"}>
               <Skill iconId = {"code"} title={"html5"} description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"}/>
               <Skill iconId = {"css"} title={"css"} description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"}/>
               <Skill iconId = {"react"} title={"react"} description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"}/>
