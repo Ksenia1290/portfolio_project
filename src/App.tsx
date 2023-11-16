249e5cb1f0cdabfe6be8fda66758bfeedcdb7e6e
Ksenia1290 <Ksu.tender@yandex.ru>
2023-11-17 00:12:51 +0300

    1

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Main } from './layout/sections/main/Main';
 import { Skill } from './layout/sections/skills/skill/Skill';
 import { Skills } from './layout/sections/skills/Skills';
 import { Works } from './layout/sections/works/Works';
+import { Testimony } from './layout/sections/testimony/Testimony';
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
           <Main/>
           <Skills/>
           <Works/>
+          <Testimony/>
         </div>
     );
 }
