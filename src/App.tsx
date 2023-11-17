1c4bb345cb6cf2c54c3fe6b2bb90f421b8f00572
Ksenia1290 <Ksu.tender@uandex.ru>
2023-11-17 22:02:12 +0300

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import './App.css';
-import styled from 'styled-components';
+import styled from "styled-components";
 import { Header } from './layout/header/Header';
 import { Main } from './layout/sections/main/Main';
-import { Skill } from './layout/sections/skills/skill/Skill';
 import { Skills } from './layout/sections/skills/Skills';
 import { Works } from './layout/sections/works/Works';
 import { Testimony } from './layout/sections/testimony/Testimony';
