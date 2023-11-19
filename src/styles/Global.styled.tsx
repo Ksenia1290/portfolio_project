786e7db49b2888b58d86f4d8dfa0aa714433b18a
Ksenia1290 <Ksu.tender@uandex.ru>
2023-11-19 21:32:33 +0300

    1

--- src/styles/Global.styled.tsx
+++ src/styles/Global.styled.tsx
@@ -12,11 +12,23 @@ export const GlobalStyle = createGlobalStyle`
     
     body {
   margin: 0;
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+  font-family: 'Poppins', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
     'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
     sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
 
+a{
+    text-decoration:none;
+}
+
+ul{
+    list-style:none;
+}
+
+button{
+    background-color:unset;
+    border:none;
+}
 `
