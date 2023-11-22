467691560f74da67d4630e72c0ef5d7664f8f0cc
Ksenia1290 <Ksu.tender@uandex.ru>
2023-11-22 23:01:07 +0300

    1

--- src/styles/Global.styled.tsx
+++ src/styles/Global.styled.tsx
@@ -19,6 +19,7 @@ export const GlobalStyle = createGlobalStyle`
     sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  color:${theme.colors.font};
 }
 
 a{
