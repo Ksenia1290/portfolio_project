955e76adf821d4589fc620a7232202ec1b9531ed
Ksenia1290 <Ksu.tender@uandex.ru>
2023-11-25 00:05:58 +0300

    1

--- src/layout/sections/skills/skill/Skill.tsx
+++ src/layout/sections/skills/skill/Skill.tsx
@@ -14,7 +14,9 @@ export const Skill = (props: SkillPropsType) =>{
     return(
 <StyledSkill>
     <FlexWrapper direction={"column"} align={"center"}>
-    <Icon iconId={props.iconId }/>
+       <IconWrapper>
+         <Icon iconId={props.iconId }/>
+       </IconWrapper>
     <SkillTitle>{props.title}</SkillTitle>
      <SkillText>{props.description}</SkillText>
     </FlexWrapper>
@@ -27,10 +29,37 @@ width: 380px;
 padding: 62px 20px 40px;
 `
 
+const IconWrapper = styled.div`
+
+ position:relative;
+
+ &::before{
+    content:"";
+    display: inline-block;
+    width: 80px;
+    height: 80px;
+    background: rgba(255, 255, 255, 0.1);
+    transform: rotate(45deg) translate(-50%, -50%);
+
+    position:absolute;
+    left: 50%;
+    top: 50px;
+    transform-origin: top left;
+ }
+`
+
 const SkillTitle = styled.h3`
-margin:70px 0 15px;
+margin: 70px 0 15px;
+color: #FFF;
+font-family: Josefin Sans, sans-serif;
+font-size: 16px;
+font-weight: 700;
+letter-spacing: 1px;
+text-transform: uppercase;
 `
 
 const SkillText = styled.p`
 text-align:center;
+font-size: 14px;
+font-weight: 400;
 `
