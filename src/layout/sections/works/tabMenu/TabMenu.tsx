5dc7d7d063d3d7f9479f0349bdba4a91dc28563d
Ksenia1290 <Ksu.tender@uandex.ru>
2023-11-26 00:03:57 +0300

    1

--- src/layout/sections/works/tabMenu/TabMenu.tsx
+++ src/layout/sections/works/tabMenu/TabMenu.tsx
@@ -1,24 +1,49 @@
 import React from "react";
 import { styled } from "styled-components";
+import { theme } from "../../../../styles/Theme";
 
 
 export const TabMenu = (props: {menuItems:Array<string>}) => {
     return (
-    <StyledMenu>
+    <StyledTabMenu>
         <ul>
             {props.menuItems.map((item:string,index:number)=> {
-             return <li key={index}>
-                       <a href="">{item}</a>
-                   </li>
+             return <ListItem key={index}>
+                       <Link href="">{item}</Link>
+                   </ListItem>
             })}
         </ul>
-    </StyledMenu> 
+    </StyledTabMenu> 
     );
 };
-const StyledMenu = styled.nav
-`
-ul{
+
+
+const StyledTabMenu = styled.nav`
+ul {
     display: flex;
     gap:30px;
+    justify-content:center;
+}
+`
+const ListItem = styled.li`
+position: relative;
+`
+const Link = styled.a`
+font-size: 14px;
+font-weight: 400;
+letter-spacing: 1px;
+text-transform: uppercase;
+padding:10px;
+
+&::before{
+    content:"";
+    display:inline-block;
+    height:10px;
+    background-color:${theme.colors.accent};
+
+    position:absolute;
+    bottom:-5px;
+    left: 0;
+    right: 0;
 }
 `
