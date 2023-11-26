d1f59b7d92811ca3454deff19dc564530fe7a908
Ksenia1290 <Ksu.tender@uandex.ru>
2023-11-26 21:37:21 +0300

    1

--- src/layout/sections/works/tabMenu/TabMenu.tsx
+++ src/layout/sections/works/tabMenu/TabMenu.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { styled } from "styled-components";
-import { theme } from "../../../../styles/Theme";
+import { Link } from "../../../../componens/ Link";
 
 
 export const TabMenu = (props: {menuItems:Array<string>}) => {
@@ -31,28 +31,4 @@ position: relative;
 z-index: 0;
 `
 
-const Link = styled.a`
-font-size: 14px;
-font-weight: 400;
-letter-spacing: 1px;
-text-transform: uppercase;
-padding:10px;
 
-&:hover{
-    &::before{
-        height:10px;  
-    }
-}
-
-&::before{
-    content:"";
-    display:inline-block;
-    background-color:${theme.colors.accent};
-
-    position:absolute;
-    bottom:-5px;
-    left: 0;
-    right: 0;
-    z-index:-1;
-}
-`
