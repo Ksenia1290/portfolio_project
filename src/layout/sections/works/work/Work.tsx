d6cb5d49ad268222046f608f9942f553ad653fa7
Ksenia1290 <Ksu.tender@uandex.ru>
2023-11-26 21:55:12 +0300

    1

--- src/layout/sections/works/work/Work.tsx
+++ src/layout/sections/works/work/Work.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { Link } from "../../../../componens/ Link";
 
 type WorkPropsType = {
 title:string
@@ -13,17 +14,22 @@ export const Work = (props:WorkPropsType) => {
     <Image src={props.src} alt=""/>
     <Title>{props.title}</Title>
     <Text>{props.text}</Text>
-    <Link href={"#"}>demo</Link>
-    <Link href={"#"}>code</Link>
+    <link href={"#"}>demo</link>
+    <link href={"#"}>code</link>
 </StyledWork>
     );
 };
 
 const StyledWork = styled.div`
     background-color: #ffe869;
+    max-width:540px;
+    width:100%;
 `
 
 const Image =styled.img`
+width:100%;
+height:260px;
+object-fit:cover;
     
 `
 const Link =styled.a`
