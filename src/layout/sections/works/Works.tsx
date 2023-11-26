56ea0f135b512c23ff4388d595c4c10750246df0
Ksenia1290 <Ksu.tender@uandex.ru>
2023-11-27 00:19:30 +0300

    1

--- src/layout/sections/works/Works.tsx
+++ src/layout/sections/works/Works.tsx
@@ -18,7 +18,7 @@ export const Works = () =>{
      <Container>
      <SectionTitle>My Works</SectionTitle>
       <TabMenu menuItems={worksItems}/>
-      <FlexWrapper justify={"space-between"}>
+      <FlexWrapper justify={"space-between"} align={"flex-statr"}>
         <Work title={"Social Network"} 
               src={socialImg}
               text={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit."}/>
