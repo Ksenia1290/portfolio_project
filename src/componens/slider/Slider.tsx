c8409a8cd07056ba650a96fcebfc2a733e03eab9
Ksenia1290 <Ksu.tender@uandex.ru>
2023-11-27 19:33:46 +0300

    1

--- src/componens/slider/Slider.tsx
+++ src/componens/slider/Slider.tsx
@@ -37,14 +37,24 @@ const Text = styled.p`
     
 `
 const Name = styled.span`
-    
+font-family: Josefin Sans, sans-serif;
+font-size: 16px;
+font-weight: 600;
+letter-spacing: 1px;
+text-transform: uppercase;
+margin:22px 0 42px;
+display:inline-block;
 `
 const Pagination = styled.div`
     span {
       display:inline-block; 
-      width:10px;
-      height:10px; 
-      margin:5px;
-      background-color: deeppink;
+      width:7px;
+      height:7px; 
+      background-color: rgba(255,255,255,0.5);
+      border-radius:20px;
+
+      & + span{
+        margin-left:5px;
+      }
     }
 `
