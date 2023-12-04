8416769e175acfa7c26d93e7cb97aa84cf2a3c25
Ksenia1290 <Ksu.tender@uandex.ru>
2023-12-04 16:30:42 +0300

    1

--- src/styles/Common.ts
+++ src/styles/Common.ts
@@ -6,12 +6,14 @@ type FontPropsType={
     weight?:number
     color? : string
     lineHeight?:number
+    Fmin: number
+    Fmax: number
 }
 
-export const font = ({family,weight,color,lineHeight}: FontPropsType) =>`
+export const font = ({family,weight,color,lineHeight, Fmax, Fmin}: FontPropsType) =>`
 font-family: ${family || "Poppins"};
 font-weight: ${weight || 400};
 color: ${color || theme.colors.font};
 line-height: ${lineHeight || 1.2};
-font-size: calc( (100vw - 480px)/(1280 - 480) * (24 - 16) + 16px);
+font-size: calc( (100vw - 360px)/(1440 - 360) * (${Fmax} - ${Fmin}) + ${Fmin}px);
 `
