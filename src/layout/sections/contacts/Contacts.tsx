e14eafe52676a68f96b94181a1f1a020d4e8c551
Ksenia1290 <Ksu.tender@uandex.ru>
2023-12-06 23:40:21 +0300

    1

--- src/layout/sections/contacts/Contacts.tsx
+++ src/layout/sections/contacts/Contacts.tsx
@@ -1,68 +1,22 @@
 import React from "react";
-import styled from "styled-components";
 import { SectionTitle } from "../../../componens/SectionTitle";
 import { Button } from "../../../componens/Button";
 import { Container } from "../../../componens/Container";
-import { theme } from "../../../styles/Theme";
+import {S} from "./Contacts_Styles";
 
-export const Contact = () =>{
+export const Contact React.FC = () =>{
     return(
-       <StyledContacts>
+       <S.Contacts>
          <Container>
          <SectionTitle>Contact</SectionTitle>
-            <StyledForm>
-               <Field placeholder={"name"}/>
-               <Field placeholder={"subject"}/>
-               <Field placeholder={"message"} as = {"textarea"}/>
+            <S.Form>
+               <S.Field placeholder={"name"}/>
+               <S.Field placeholder={"subject"}/>
+               <S.Field placeholder={"message"} as = {"textarea"}/>
                <Button type={"submit"}>Send message</Button>
-            </StyledForm> 
+            </S.Form> 
          </Container>
-       </StyledContacts>
+       </S.Contacts>
     );
 };
 
-const StyledContacts = styled.section`
-   
-`
-
-
-const StyledForm = styled.form`
-    max-width: 540px;
-    width:100%;
-    display:flex;
-    flex-direction:column;
-    align-items: center;
-    gap:16px;
-    margin: 0 auto;
-
-
-    textarea{
-       resize:none;
-       height:155px;
-    }
-    `
-
-
-const Field = styled.input`
-    width:100%;
-    border: 1px solid ${theme.colors.borderColor};
-    background-color: ${theme.colors.secondaryBg};
-    padding:7px 15px;
-
-    
-font-family: 'Poppins', sans-serif;
-font-size: 12px;
-font-weight: 400;
-letter-spacing: 0.6px;
-
-color:${theme.colors.font};
-
-&::placeholder{
-   color: ${theme.colors.placeholdeColor};
-   text-transform: capitalize;
-}
-
-&:focus-visible {
-   outline: 1px solid ${theme.colors.borderColor};
-}
-`
