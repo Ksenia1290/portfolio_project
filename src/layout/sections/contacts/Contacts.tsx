d0f080a7b9d949bbaa22c1ec7a04f3ae14a68c46
Ksenia1290 <Ksu.tender@uandex.ru>
2023-12-09 03:33:15 +0300

    1

--- src/layout/sections/contacts/Contacts.tsx
+++ src/layout/sections/contacts/Contacts.tsx
@@ -13,7 +13,7 @@ export const Contact: React.FC = () =>{
 
     if(!form.current)return
 
-    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
+    emailjs.sendForm('service_yqq349p', 'template_r00qp29', form.current, 'm5qnJ7YS6rVBF-cL3')
       .then((result) => {
           console.log(result.text);
       }, (error) => {
@@ -24,7 +24,7 @@ export const Contact: React.FC = () =>{
        <S.Contacts>
          <Container>
          <SectionTitle>Contact</SectionTitle>
-            <S.Form>
+            <S.Form ref={form} onSubmit={sendEmail}>
                <S.Field placeholder={"name"}/>
                <S.Field placeholder={"subject"}/>
                <S.Field placeholder={"message"} as = {"textarea"}/>
