a7bfb38010af24242d1895a088edf8bf6dc4d0a4
Ksenia1290 <Ksu.tender@uandex.ru>
2023-12-09 18:21:07 +0300

    1

--- src/componens/slider/Slider.tsx
+++ src/componens/slider/Slider.tsx
@@ -1,22 +1,50 @@
-import React from "react"
-import { FlexWrapper } from "../FlexWrapper";
-import {S} from "./Slider_Styles";
+import React from 'react';
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
 
-export const Slider: React.FC = () => {
-    return (
-        <S.Slider>
-            <FlexWrapper>
-            <S.Slide>
-                <S.Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</S.Text>
-                <S.Name>@ivan ivanow</S.Name>
-             </S.Slide>
-            </FlexWrapper>
-             <S.Pagination>
-              <span> </span>
-              <span className={"active"}></span>
-              <span> </span>
-             </S.Pagination>
-        </S.Slider>
-    );
+const responsive = {
+    0: { items: 1 },
+    568: { items: 2 },
+    1024: { items: 3 },
 };
+
+const items = [
+    <div className="item" data-value="1">1</div>,
+    <div className="item" data-value="2">2</div>,
+    <div className="item" data-value="3">3</div>,
+    <div className="item" data-value="4">4</div>,
+    <div className="item" data-value="5">5</div>,
+];
+
+const Carousel = () => (
+    <AliceCarousel
+        mouseTracking
+        items={items}
+        responsive={responsive}
+        controlsStrategy="alternate"
+    />
+);
+
+
+//import React from "react"
+//import { FlexWrapper } from "../FlexWrapper";
+//import {S} from "./Slider_Styles";
+
+//export const Slider: React.FC = () => {
+//    return (
+//        <S.Slider>
+//            <FlexWrapper>
+ //           <S.Slide>
+   //             <S.Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</S.Text>
+   //             <S.Name>@ivan ivanow</S.Name>
+  //           </S.Slide>
+  //          </FlexWrapper>
+  //           <S.Pagination>
+  //            <span> </span>
+ //             <span className={"active"}></span>
+  //            <span> </span>
+ //            </S.Pagination>
+  //      </S.Slider>
+ //   );
+//};
 
