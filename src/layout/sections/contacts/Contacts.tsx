8e1286b8378406e0e0a9ae75235d61930f1263cb
Ksenia1290 <Ksu.tender@uandex.ru>
2023-12-09 03:23:21 +0300

    1

--- src/layout/sections/contacts/Contacts.tsx
+++ src/layout/sections/contacts/Contacts.tsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useRef } from "react";
 import { SectionTitle } from "../../../componens/SectionTitle";
 import { Button } from "../../../componens/Button";
 import { Container } from "../../../componens/Container";
 import {S} from "./Contacts_Styles";
+import emailjs from '@emailjs/browser';
 
 export const Contact: React.FC = () =>{
+   const form = useRef();
+
+  const sendEmail = (e:any) => {
+    e.preventDefault();
+
+    if(!form.current)return
+    
+    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
     return(
        <S.Contacts>
          <Container>
